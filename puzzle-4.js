7f79e1f9a1283670f48817eea9a904baa0c95855
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-01-31 08:33:39 -0800

    Puzzle 4

---
+++ puzzle-4.js
@@ -0,0 +1,20 @@
+right();
+function downFourTimes() {
+  down();
+  down();
+  down();
+  down();
+}
+downFourTimes();
+var firstColorSeen = getColor();
+down();
+setColor(firstColorSeen);
+right();
+right();
+up();
+right();
+var secondColorSeen = getColor();
+down();
+setColor(secondColorSeen);
+right();
+right();
