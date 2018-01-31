c45a3ba87450ec170a8c03253ee97cdf5252596f
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-01-31 08:34:16 -0800

    Puzzle 5

---
+++ puzzle-5.js
@@ -0,0 +1,33 @@
+function rightTwoTimes() {
+  right();
+  right();
+}
+rightTwoTimes();
+if (getColor() == "blue") {
+   down();
+} else {
+   up();
+}
+function rightTwoTimes() {
+  right();
+  right();
+}
+rightTwoTimes();
+if (getColor() == "blue") {
+   down();
+} else {
+   up();
+}
+function rightTwoTimes() {
+  right();
+  right();
+}
+rightTwoTimes();
+if (getColor() == "blue") {
+   down();
+} else {
+   up();
+}
+right();
+right();
+right();
