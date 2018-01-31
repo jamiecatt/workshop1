0203d49b574a7dc8d6cdaee25cc372766c99ca9c
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-01-31 08:35:35 -0800

    Puzzle 7

---
+++ puzzle-7.js
@@ -0,0 +1,33 @@
+while (getColor() != "red") {
+   down();
+}
+function rightTwoTimes() {
+  right();
+  right();
+}
+rightTwoTimes();while (getColor() != "red") {
+   up();
+}
+function rightTwoTimes() {
+  right();
+  right();
+}
+rightTwoTimes();while (getColor() != "red") {
+   down();
+}
+function rightTwoTimes() {
+  right();
+  right();
+}
+rightTwoTimes();while (getColor() != "red") {
+   up();
+}
+function rightTwoTimes() {
+  right();
+  right();
+}
+rightTwoTimes();
+while (getColor() != "red") {
+   down();
+}
+right();
