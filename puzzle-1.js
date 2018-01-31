2dc8670dd8d8454323355538a19279984ce0b652
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-01-31 08:31:50 -0800

    Puzzle 1

---
+++ puzzle-1.js
@@ -0,0 +1,28 @@
+function downSevenTimes() {
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+}
+downSevenTimes();
+function rightFiveTimes() { 
+  right();
+  right();
+  right();
+  right();
+  right();
+}
+rightFiveTimes();
+function upTwoTimes() {
+  up();
+  up();
+}
+upTwoTimes();
+function rightTwoTimes() {
+  right();
+  right();
+}
+rightTwoTimes();
