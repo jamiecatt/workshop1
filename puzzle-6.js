7a0c574289d405ada7c51a4dcb9bf71d208334e0
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-01-31 08:34:51 -0800

    Puzzle 6

---
+++ puzzle-6.js
@@ -0,0 +1,23 @@
+right();
+if (getColor() == "red") {
+   up();
+   up();
+   up();
+   left();
+   up();
+} else {
+if (getColor() == "blue") {
+   down();
+   down();
+   down();
+}
+}
+if (getColor() =="blue") {
+   right();
+   down();
+} else {
+if (getColor() == "red") {
+   left();
+   down();
+} 
+}
