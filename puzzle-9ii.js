fbd7b665857d8305d7e5848504a2e02b405bd3eb
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-01-31 08:37:53 -0800

    Puzzle 9

---
+++ puzzle-9ii.js
@@ -0,0 +1,143 @@
+var steps = 0
+while (steps < 20) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 20) {
+  up();
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 20) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 20) {
+  up();
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 20) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 20) {
+  up();
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 20) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 20) {
+  up();
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 20) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 20) {
+  up();
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 20) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 20) {
+  up();
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 20) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 20) {
+  up();
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 20) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 20) {
+  up();
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 20) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 20) {
+  up();
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 20) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
