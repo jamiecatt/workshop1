62c33fa6fd73fde818ddbb7e454a8d88bf3318e8
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-01-31 08:33:05 -0800

    Puzzle 3

---
+++ puzzle-3.js
@@ -0,0 +1,24 @@
+function downTwoTimes() {
+  down();
+  down();
+}
+downTwoTimes();
+var firstColorSeen = getColor();
+function downThreeTimes() {
+  down();
+  down();
+  down();
+}
+downThreeTimes();
+function rightTwoTimes() { 
+  right();
+  right();
+}
+rightTwoTimes();
+setColor(firstColorSeen);
+function rightTwoTimes() { 
+  right();
+  right();
+}
+rightTwoTimes();
+up();
