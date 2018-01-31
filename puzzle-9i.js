88500eed64b8239477dd6c8eaa192375e7876b29
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-01-31 08:37:03 -0800

    Puzzle 9

---
+++ puzzle-9i.js
@@ -0,0 +1,156 @@
+var steps = 0
+while (steps < 9) {
+  right();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  left();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  up();
+  setColor("red");
+  steps = steps + 1
+}
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 9) {
+  right();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  left();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  up();
+  setColor("red");
+  steps = steps + 1
+}
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 9) {
+  right();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  left();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  up();
+  setColor("red");
+  steps = steps + 1
+}
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+var steps = 0
+while (steps < 9) {
+  right();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  down();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  left();
+  setColor("red");
+  steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+  up();
+  setColor("red");
+  steps = steps + 1
+}
